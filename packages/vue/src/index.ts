b3b5079ee0b10fcda70ee2f0705cd0495e5a13b3
jiangqiang <jiangq.leaves@gmail.com>
2021-02-19 21:33:30 +0800

    refactor(vue): remove reactivity code

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -5,26 +5,17 @@ import {
   SAXONY_CORE_KEY, PROVIDER_KEY, RootProvider, RootProviderProps, ScopedProvider, ScopedProviderProps
 } from './components';
 import useService from './useService';
-import {
-  makeAutoObserver, makeObserver, AnnotationMap, AnnotationType
-} from './reactive/observe';
-import Observable from './reactive/observable';
 
 export {
   useService,
-  Observable,
-  makeAutoObserver,
-  makeObserver,
   RootProvider,
   ScopedProvider,
   withRootProvider,
   withScopedProvider,
   PROVIDER_KEY,
-  SAXONY_CORE_KEY,
-  AnnotationType
+  SAXONY_CORE_KEY
 };
 export type {
-  AnnotationMap,
   RootProviderProps,
   ScopedProviderProps
 };
