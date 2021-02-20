9c02fc38e6993ad92865f30eba1d0441797b255c
jiangqiang <jiangq.leaves@gmail.com>
2021-02-20 11:03:10 +0800

    feat(core): adds type detection method

--- packages/core/src/exception.ts
+++ packages/core/src/exception.ts
@@ -1,3 +1,11 @@
+const type = Symbol('type');
+
 export default class Exception extends Error {
   name = 'BizicException';
+
+  [type]: undefined;
+
+  static isBizicException(e: unknown): e is Exception {
+    return typeof e === 'object' && e !== null && type in e;
+  }
 }
